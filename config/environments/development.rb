4bd915be5c9fdc7fb56751e4ea31900ea4ab8eb4
sasakiiori <126736985+sasakiiori@users.noreply.github.com>
2023-06-19 07:36:01 +0000

    COOKING REVOLUSION

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "7d3cfd3cf0784ee1b376683c289ac1c1.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
