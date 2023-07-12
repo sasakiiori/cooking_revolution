61af27eebc1c9e6f6b948212e4b7cf87d5ccc793
sasakiiori <126736985+sasakiiori@users.noreply.github.com>
2023-07-12 11:46:27 +0000

    COOKING REVOLUTION

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,10 +3,19 @@ class CustomersController < ApplicationController
   end
 
   def show
-
+    @customer = Customer.find(params[:id])
   end
 
   def update
+   @customer = Customer.find(params[:id])
+   @customer.update(customer_params)
+   redirect_to customer_path(@customer)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name)
   end
 
 end
