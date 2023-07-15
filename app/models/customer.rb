c5b57391de8939dbaf4e6843123b2aa4b7d161f9
sasakiiori <126736985+sasakiiori@users.noreply.github.com>
2023-07-15 09:46:20 +0000

    COOKING REVOLUTION

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,12 @@ class Customer < ApplicationRecord
 
   has_many :posts
   has_one_attached :image
+
+  def get_profile_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/sample.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [100, 100]).processed
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   scope :publics, -> { where(status: true) }
   has_one_attached :image
   belongs_to :customer
+
+  def get_profile_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/sample.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [1000, 1000]).processed
+  end
 end
