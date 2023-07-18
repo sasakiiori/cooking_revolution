5f75b62e42c9273df2389a6aa9b0631eec65c5f5
sasakiiori <126736985+sasakiiori@users.noreply.github.com>
2023-07-18 12:36:36 +0000

    COOKING REVOLUTION

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,11 @@ class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :customer
 
-  def get_image
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/sample.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-      image.variant(resize_to_limit: [1000, 1000]).processed
+      image.variant(resize_to_limit: [width, height]).processed
   end
 end
